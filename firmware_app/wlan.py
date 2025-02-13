f4055b90f6abd658fec24b2836bc86dcaf933db8
JackDesBwa <jackdesbwa@gmail.com>
2025-02-13 15:58:32 +0100

    Replace wifi_manager by custom connect code (no AP mode)

---
+++ firmware_app/wlan.py
@@ -0,0 +1,78 @@
+import json
+import network
+import asyncio
+
+wlan = network.WLAN(network.STA_IF)
+hostname = 'haumbot'
+networks = []
+try:
+    with open('wifi.dat', 'r') as f:
+        for line in f:
+            networks.append(line.strip().split(';', 1))
+except OSError:
+    pass
+
+def print_wlaninfo():
+    if wlan.isconnected():
+        c = wlan.ifconfig()
+        print("Connected\nIP:", c[0])
+        print("Mask:", c[1])
+        print("Gateway:", c[2])
+        print("DNS:", c[3])
+        print("Hostname:", wlan.config('hostname'))
+        print(f"RSSI: {wlan.status('rssi')} dB")
+    else:
+        print("Not connected")
+
+
+async def connect(ssid, passwd):
+    wlan.connect(ssid, passwd)
+    while True:
+        status = wlan.status()
+        if status == network.STAT_CONNECTING or status == network.STAT_IDLE:
+            await asyncio.sleep_ms(10)
+        else:
+            break
+
+    if status == network.STAT_ASSOC_FAIL:
+        print('Wifi: ASSOC_FAIL')
+    elif status == network.STAT_BEACON_TIMEOUT:
+        print('Wifi: BEACON_TIMEOUT')
+    elif status == network.STAT_GOT_IP:
+        print('Wifi: GOT_IP')
+    elif status == network.STAT_HANDSHAKE_TIMEOUT:
+        print('Wifi: HANDSHAKE_TIMEOUT')
+    elif status == network.STAT_NO_AP_FOUND:
+        print('Wifi: NO_AP_FOUND')
+    elif status == network.STAT_WRONG_PASSWORD:
+        print('Wifi: WRONG_PASSWORD')
+
+    print_wlaninfo()
+
+
+async def autoconnect():
+    if wlan.isconnected():
+        print_wlaninfo()
+
+    while True:
+        if wlan.isconnected():
+            await asyncio.sleep(5)
+            continue
+        if not wlan.active():
+            wlan.active(True)
+        if wlan.status() != network.STAT_IDLE:
+            wlan.disconnect()
+        if hostname:
+            wlan.config(hostname=hostname)
+        nets = wlan.scan()
+        ssids = tuple(net[0] for net in nets)
+
+        for n in networks:
+            s = n[0].encode()
+            if s in ssids:
+                print(f'Found network "{s}", try connecting')
+                await connect(*n)
+                break
+        else:
+            print(f'No known network in {ssids}')
+        await asyncio.sleep(5)
