c7533638ea5ade558b2066ce8b2d9a85747bab88
JackDesBwa <jackdesbwa@gmail.com>
2025-02-13 14:15:34 +0100

    Simplify web code using haum/micropython-aiowebserver

--- firmware_app/main.py
+++ firmware_app/main.py
@@ -1,13 +1,18 @@
-
-import network
-
 from wifi_manager import WifiManager
 wm = WifiManager()
 wm.connect()
-
-
-
 while not wm.is_connected():
     pass
 
-import run_server
+import asyncio
+import led
+import routes # To register web routes
+import aiowebserver as web
+
+asyncio.create_task(web.start())
+asyncio.create_task(led.update_led())
+
+try:
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    web.stop()
---
+++ firmware_app/routes.py
@@ -0,0 +1,58 @@
+import asyncio
+import json
+import led
+import aiowebserver as web
+
+@web.route('GET', '/')
+async def root_handler(rq):
+    await rq.sendfile('index.htm')
+
+
+@web.route('GET', '/api')
+async def api_get_handler(rq):
+    await rq.sendfile('api/index.htm')
+
+
+# curl -X POST -i 'http://ip_esp/api' --data '{"LED":{"r":0,"g":0,"b":0}}'
+@web.route('POST', '/api')
+async def api_post_handler(rq):
+    led_val = await rq.decode_postjson_data()
+    if not led_val:
+        await rq.return_status(400)
+        await rq.w('Wrong format')
+        return
+    try:
+        led.np[0] = (led_val['LED']['g'],led_val['LED']['r'] , led_val['LED']['b'])
+        led.new_color.set()
+        await rq.w('OK')
+    except KeyError:
+        await rq.return_status(400)
+        await rq.w('KeyError')
+
+
+@web.route('GET', '/led')
+async def led_get_handler(rq):
+    await rq.sendfile('api/led_index.htm')
+
+
+# curl -X POST -i 'http://ip_esp/led' --data 'ledcolor=#666666'
+@web.route('POST', '/led')
+async def led_post_handler(rq):
+    form = await rq.decode_postform_data()
+    color = form.get('ledcolor')
+    if not color:
+        await rq.return_status(400)
+        await rq.w('Need ledcolor')
+        return
+    try:
+        if len(color) != 7: raise ValueError
+        if color[0] != '#': raise ValueError
+        color = int(color[1:], 16)
+    except ValueError:
+        await rq.return_status(400)
+        await rq.w('Invalid value')
+        return
+    led.np[0] = ((color >> 8 & 0xFF), (color >> 16), (color & 0xFF))
+    led.new_color.set()
+    await rq.w('OK')
+
