e6b4229a47f80a4be57d5d70e31c35a83cc8a215
JackDesBwa <jackdesbwa@gmail.com>
2025-02-13 15:58:32 +0100

    Replace wifi_manager by custom connect code (no AP mode)

--- firmware_app/main.py
+++ firmware_app/main.py
@@ -1,14 +1,10 @@
-from wifi_manager import WifiManager
-wm = WifiManager()
-wm.connect()
-while not wm.is_connected():
-    pass
-
 import asyncio
+import wlan
 import led
 import routes # To register web routes
 import aiowebserver as web
 
+asyncio.create_task(wlan.autoconnect())
 asyncio.create_task(web.start())
 asyncio.create_task(led.update_led())
 
