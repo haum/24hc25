4fa6438fa91c211baf48840ad1043a3bae57cf7b
JbLb <jerome@jblb.net>
2025-02-14 19:42:45 +0100

    other way to display ip for tests

--- firmware_app/haumbot/wlan.py
+++ firmware_app/haumbot/wlan.py
@@ -26,6 +26,7 @@ def print_wlaninfo():
         print("DNS:", c[3])
         print("Hostname:", wlan.config('hostname'))
         print(f"RSSI: {wlan.status('rssi')} dB")
+        print(f'web access: http://{c[0]}')
     else:
         print("Not connected")
 
