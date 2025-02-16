da506833c0b9f25eed97657e87b625e274fd802c
JackDesBwa <jackdesbwa@gmail.com>
2025-02-17 00:03:47 +0100

    Add motors module

---
+++ firmware_app/haumbot/motors.py
@@ -0,0 +1,33 @@
+import machine
+
+class Servo:
+    def __init__(self, pin, inv=False):
+        self.inv = -1 if inv else 1
+        self.pwm = machine.PWM(pin, freq=50, duty_ns=self._p(0))
+        self.stop()
+
+    def _p(self, p):
+        return int(min(max(-1.0, p*self.inv), 1.0) * 265000 + 1415000)
+
+    def _start(self, p=0):
+        self.pwm.init(freq=50, duty_ns=self._p(p))
+        self.on = True
+
+    def stop(self):
+        self.pwm.deinit()
+        self.on = False
+
+    def go(self, p):
+        if not self.on:
+            if p != 0:
+                self._start(p)
+        elif p == 0:
+            self.stop()
+        else:
+            self.pwm.duty_ns(self._p(p))
+
+pin_ml = machine.Pin(5, machine.Pin.OUT)
+pin_mr = machine.Pin(6, machine.Pin.OUT)
+ml = Servo(pin_ml, False)
+mr = Servo(pin_mr, True)
+
