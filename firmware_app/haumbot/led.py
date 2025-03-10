420509f76ab7e565e375b6f5ed2040044241026a
JackDesBwa <jackdesbwa@gmail.com>
2025-03-10 12:36:54 +0100

    Fix code style in haumbot/led.py

--- firmware_app/haumbot/led.py
+++ firmware_app/haumbot/led.py
@@ -6,13 +6,11 @@ from neopixel import NeoPixel
 np = NeoPixel(Pin(conf.get('pin_led'), Pin.OUT), 1)  # create NeoPixel driver for 1 pixels on pin_led
 
 np[0] = (0, 0, 0)
-np.write()         
+np.write()
 new_color = asyncio.Event()
 
 async def update_led():
     while True:
-        # wait for the event to be set
-        await new_color.wait()
+        await new_color.wait() # wait for the event to be set
         np.write()
         new_color.clear()
-
