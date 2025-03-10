4d810daff023f69c41edac9c50bbeae3bc03252b
JackDesBwa <jackdesbwa@gmail.com>
2025-03-10 12:36:15 +0100

    Add config file

--- firmware_app/haumbot/position.py
+++ firmware_app/haumbot/position.py
@@ -1,6 +1,7 @@
 import micropython
 import machine
 import struct
+import haumbot.config as conf
 from math import cos, sin
 
 period = micropython.const(50) # ms
@@ -12,9 +13,18 @@ Kv, Kw = 1, 1 # Constants for speed and angle
 x, y, a = 0, 0, 0 # Position in m and rad
 v = 0 # average speed
 
-P = machine.Pin
-i2c_l = machine.SoftI2C(scl=P(10), sda=P(9), freq=400_000, timeout=1000)
-i2c_r = machine.SoftI2C(scl=P(7), sda=P(6), freq=400_000, timeout=1000)
+i2c_l = machine.SoftI2C(
+    scl=machine.Pin(conf.get('pin_i2c_l_scl')),
+    sda=machine.Pin(conf.get('pin_i2c_l_sda')),
+    freq=400_000,
+    timeout=1000
+)
+i2c_r = machine.SoftI2C(
+    scl=machine.Pin(conf.get('pin_i2c_r_scl')),
+    sda=machine.Pin(conf.get('pin_i2c_r_sda')),
+    freq=400_000,
+    timeout=1000
+)
 timer = machine.Timer(0)
 
 def as5600_get_rawangle(i2c):
@@ -45,11 +55,13 @@ def position_tick(p=None):
     owl, owr = wl, wr
 
 def reset():
-    global iwl, iwr, owl, owr, tl, tr, x, y
+    global iwl, iwr, owl, owr, tl, tr, x, y, Kw, Kv
     iwl, iwr = as5600_get_rawangle(i2c_l), as5600_get_rawangle(i2c_r)
     owl, owr = read_angles()
     tl, tr = 0, 0
     x, y = 0, 0
+    Kw = conf.get('Kw')
+    Kv = conf.get('Kv')
 
 def start_measure():
     try:
@@ -57,11 +69,6 @@ def start_measure():
     except OSError:
         print('At least one AS5600 not found, not measuring position')
         return
-
-    # TODO Read from config file
-    global Kw, Kv
-    Kw = 0.00073885
-    Kv = 0.0005
 
     timer.init(
         period=period,
