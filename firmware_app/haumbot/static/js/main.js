a3f0038d1fb8a5afd20483882bb30afc003ef224
JackDesBwa <jackdesbwa@gmail.com>
2025-03-16 20:21:29 +0100

    Add fullscreen and close buttons in section title

--- firmware_app/haumbot/static/js/main.js
+++ firmware_app/haumbot/static/js/main.js
@@ -14,8 +14,14 @@ if (sessionStorage.getItem('sections_opened')) {
 }
 for (const s of sections) {
 	const h2 = document.createElement('h2');
+	const b_spans = document.createElement('b');
+	const span_fs = document.createElement('span');
+	span_fs.innerText = "⛶";
+	const span_close = document.createElement('span');
+	span_close.innerText = "×";
+	b_spans.append(span_fs, span_close);
 	const title = s.dataset.title;
-	h2.innerText = title;
+	h2.append(title, b_spans);
 	s.insertBefore(h2, s.firstChild);
 	s.classList.add('pico');
 	const li = document.createElement('li');
@@ -25,20 +31,32 @@ for (const s of sections) {
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 	label.append(checkbox, title);
-	checkbox.addEventListener('click', e => {
-		const on = e.target.checked;
+	const toggle = on => {
 		s.classList.toggle('opened', on);
+		checkbox.checked = on;
 		if (on)
 			sections_opened.add(title);
 		else
 			sections_opened.delete(title);
 		sessionStorage.setItem('sections_opened', Array.from(sections_opened).join('§'));
-	});
+	}
+	checkbox.addEventListener('click', e => toggle(e.target.checked));
 	checkbox.checked = false;
 	if (sections_opened.has(title)) {
 		s.classList.add('opened');
 		checkbox.checked = true;
 	}
+	span_close.addEventListener('click', () => {
+		if (document.fullscreenElement != null)
+			document.exitFullscreen();
+		toggle(false);
+	});
+	span_fs.addEventListener('click', () => {
+		if (document.fullscreenElement == null)
+			s.requestFullscreen();
+		else
+			document.exitFullscreen();
+	});
 }
 
 export async function form_post(form) {
