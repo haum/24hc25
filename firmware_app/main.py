37c21ddc2f22245501977831bbdf64346897ca24
JackDesBwa <jackdesbwa@gmail.com>
2025-03-18 12:52:35 +0100

    Loop on app to avoid people to accidently enter REPL on tty (but add a loophole)

--- firmware_app/main.py
+++ firmware_app/main.py
@@ -2,4 +2,13 @@ import webrepl
 webrepl.start(32, 'haumbot')
 
 import haumbot
-haumbot.start()
+from time import sleep
+while True:
+    try:
+        haumbot.start()
+    except:
+        pass
+    try:
+        sleep(0.5)
+    except KeyboardInterrupt:
+        break
