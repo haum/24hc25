e24dc5e1020484bfa3276a3286b8e802a2a96c8f
JackDesBwa <jackdesbwa@gmail.com>
2025-03-19 22:50:59 +0100

    Add wheels position display

--- firmware_app/haumbot/static/js/main.js
+++ firmware_app/haumbot/static/js/main.js
@@ -3,6 +3,7 @@ import './led.js'
 import './motors.js'
 import './position.js'
 import './config.js'
+import './wheels.js'
 import './wlan.js'
 
 const sections = document.querySelectorAll('section[data-title]');
---
+++ firmware_app/haumbot/static/js/wheels.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('wheels');
+const text_color = getComputedStyle(canvas).color;
+
+document.body.addEventListener('infos_wheels', e => {
+	const ctx = canvas.getContext("2d");
+	const w = canvas.width;
+	const h = canvas.height;
+	const r = 195;
+	const wl = e.detail[0]/2048*Math.PI;
+	const wr = e.detail[1]/2048*Math.PI;
+	const tl = e.detail[2];
+	const tr = e.detail[3];
+	ctx.clearRect(0, 0, w, h);
+
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = "#808080"
+
+	ctx.beginPath();
+	ctx.arc(w/4, h/2, r, 0, 2 * Math.PI, false);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.arc(3*w/4, h/2, r, 0, 2 * Math.PI, false);
+	ctx.stroke();
+
+	ctx.save();
+	ctx.translate(w/4, h/2)
+	ctx.rotate(wl)
+	ctx.beginPath()
+	ctx.moveTo(0, 0);
+	ctx.lineTo(r, 0);
+	ctx.stroke();
+	ctx.restore()
+
+	ctx.save();
+	ctx.translate(3*w/4, h/2)
+	ctx.rotate(wr)
+	ctx.beginPath()
+	ctx.moveTo(0, 0);
+	ctx.lineTo(r, 0);
+	ctx.stroke();
+	ctx.restore()
+
+	ctx.font = '30px sans-serif';
+	ctx.fillStyle = text_color;
+	ctx.textAlign = 'center';
+	if (tl) ctx.fillText(tl, w/4, 3*h/4);
+	if (tr) ctx.fillText(tr, 3*w/4, 3*h/4);
+});
