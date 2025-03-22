db74b25dda289584b9484e9639cec13385a58b73
JackDesBwa <jackdesbwa@gmail.com>
2025-03-22 13:26:49 +0100

    Use rangefinder config values in rangefinder task

--- firmware_app/haumbot/rangefinder.py
+++ firmware_app/haumbot/rangefinder.py
@@ -2,6 +2,7 @@ import asyncio
 import machine
 import vl53l3cx
 import haumbot.config as conf
+from math import exp
 
 i2c = machine.I2C(
     0,
@@ -13,16 +14,19 @@ i2c = machine.I2C(
 
 vl = vl53l3cx.VL53L3CX(i2c)
 distance = 0
+etau = 0.82
+ms = 10
 
 async def run():
-    global distance
+    global distance, etau
     is_present = (vl.getSensorId() == 0xeaaa)
     if is_present:
+        etau = exp(-ms/max(conf.get('rangefinder_tau'), 1))
         vl.sensorInit()
-        vl.setSigmaThreshold(30)
-        vl.setSignalThreshold(2000)
+        vl.setSigmaThreshold(conf.get('rangefinder_sigma'))
+        vl.setSignalThreshold(conf.get('rangefinder_thresh'))
         vl.setInterruptConfiguration(0, False)
-        vl.setInterMeasurementInMs(20)
+        vl.setInterMeasurementInMs(ms)
         vl.setRoi(16)
         vl.startRanging()
 
@@ -32,7 +36,7 @@ async def run():
                 data = vl.dumpDebugData()
                 measurement_status, estimated_distance_mm, signal_kcps, sigma_mm, ambient_kcps = data
                 if measurement_status == 0:
-                    distance = estimated_distance_mm + 0.82 * (distance - estimated_distance_mm)
-            await asyncio.sleep_ms(20)
+                    distance = estimated_distance_mm + etau * (distance - estimated_distance_mm)
+            await asyncio.sleep_ms(ms)
     else:
         print('Sensor not found.')
