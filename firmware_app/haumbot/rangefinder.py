91bc68c830c0da1633ff79299e3d2af371c7124b
JackDesBwa <jackdesbwa@gmail.com>
2025-03-22 15:50:49 +0100

    Update linear correction of rangefinder live

--- firmware_app/haumbot/rangefinder.py
+++ firmware_app/haumbot/rangefinder.py
@@ -14,16 +14,15 @@ i2c = machine.I2C(
 
 vl = vl53l3cx.VL53L3CX(i2c)
 distance = 0
-etau = 0.82
+etau = 0
 ms = 10
 lin_a = conf.get('rangefinder_lin_a')
 lin_b = conf.get('rangefinder_lin_b')
 
 async def run():
-    global distance, etau
+    global distance
     is_present = (vl.getSensorId() == 0xeaaa)
     if is_present:
-        etau = exp(-ms/max(conf.get('rangefinder_tau'), 1))
         vl.sensorInit()
         vl.setSigmaThreshold(conf.get('rangefinder_sigma'))
         vl.setSignalThreshold(conf.get('rangefinder_thresh'))
@@ -43,3 +42,16 @@ async def run():
             await asyncio.sleep_ms(ms)
     else:
         print('Sensor not found.')
+
+def conf_changed():
+    global lin_a, lin_b, etau
+    lin_a = conf.get('rangefinder_lin_a')
+    lin_b = conf.get('rangefinder_lin_b')
+    etau = exp(-ms/max(conf.get('rangefinder_tau'), 1))
+
+def start_measure():
+    conf.notify_changes('rangefinder_lin_a', conf_changed)
+    conf.notify_changes('rangefinder_lin_b', conf_changed)
+    conf.notify_changes('rangefinder_tau', conf_changed)
+
+conf_changed()
